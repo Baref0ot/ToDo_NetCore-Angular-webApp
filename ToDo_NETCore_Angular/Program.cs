c7479592132d555771458a7ee413cec4f0dc0d78
baref0ot <Mattwright700@yahoo.com>
2023-12-12 00:34:02 -0500

    Added Newton JSON Serializer to middleware pipeline. - Reference loop handling Ignored, to prevent endless loops when the JSON serializer attempts to navigate object relationships where two objects reference eachother directly or indirectly. - Contract Resolver set to Default, specifies how Newtonsoft should map .NET objects to JSON and vice versa. Example, using the public properties of objects for serialization and deserialization.

--- ToDo_NETCore_Angular/Program.cs
+++ ToDo_NETCore_Angular/Program.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//JSON Serializer
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options => 
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+    .AddNewtonsoftJson(options => 
+        options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
 
 var app = builder.Build();
 
