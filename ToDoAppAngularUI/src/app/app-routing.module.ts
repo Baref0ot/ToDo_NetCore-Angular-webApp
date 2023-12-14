8ca5185cdc4da14a793810f2c0466026d2ceef36
baref0ot <Mattwright700@yahoo.com>
2023-12-14 06:45:17 -0500

    Configure modules and routing for components.

--- ToDoAppAngularUI/src/app/app-routing.module.ts
+++ ToDoAppAngularUI/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoteListComponent } from './note-list/note-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'notes', component: NoteListComponent},
+  {path: '', redirectTo: 'notes', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ToDoAppAngularUI/src/app/app.module.ts
+++ ToDoAppAngularUI/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { NoteListComponent } from './note-list/note-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
