4a4b0437cad989b6c0f645f86f793edc6441faee
baref0ot <Mattwright700@yahoo.com>
2023-12-14 05:43:30 -0500

    Create a Note model/class that will represent data coming from and to the WebAPI

--- ToDoAppAngularUI/src/app/app.component.ts
+++ ToDoAppAngularUI/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'ToDoAppAngularUI';
 }
---
+++ ToDoAppAngularUI/src/app/models/note.model.ts
@@ -0,0 +1,4 @@
+export class Note {
+    id?: number;
+    note: string = '';
+}
