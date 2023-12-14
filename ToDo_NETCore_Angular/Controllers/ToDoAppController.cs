c0f2c292d6c2eaa2ce7f0466d549a600aae6310b
baref0ot <Mattwright700@yahoo.com>
2023-12-13 22:55:18 -0500

    - Changed <InvariantGlobalization> to false in project file. - Established a connection with the database. - Added HttpGet to retrieve To Do notes from the database.

--- ToDo_NETCore_Angular/Controllers/ToDoAppController.cs
+++ ToDo_NETCore_Angular/Controllers/ToDoAppController.cs
@@ -1,9 +1,48 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
 
 namespace ToDo_NETCore_Angular.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class ToDoAppController : ControllerBase {
+
+        // data fields
+        private IConfiguration _configuration;
+
+        // setting up dependency injection
+        public ToDoAppController(IConfiguration configuration) {
+            _configuration = configuration; 
+        }
+
+        [HttpGet]
+        [Route("GetNotes")]
+        public JsonResult GetNotes() {
+
+            // create quick query
+            string query = "select * from dbo.notes";
+            DataTable table = new DataTable();
+
+            // get connection db details
+            string sqlDatasource = _configuration.GetConnectionString("ToDoAppConnection");
+
+            // setup SqlDataReader
+            SqlDataReader myReader;
+
+            using(SqlConnection myConnection = new SqlConnection(sqlDatasource)) {
+                myConnection.Open();
+
+                using(SqlCommand myCommand = new SqlCommand(query, myConnection)) {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConnection.Close();   
+                }
+            }
+
+            return new JsonResult(table);
+        }
     }
 }
