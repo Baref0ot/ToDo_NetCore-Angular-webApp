e8cceef65cc255edf684f5e46bf3e63a20698651
baref0ot <Mattwright700@yahoo.com>
2023-12-14 02:13:26 -0500

    Scaffolding of Angular front-end that will act as a user interface to consume ASP.NET Core Web APIs.

--- ToDoAppAngularUI/src/app/app.component.spec.ts
+++ ToDoAppAngularUI/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todoapp'`, () => {
+  it(`should have as title 'ToDoAppAngularUI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todoapp');
+    expect(app.title).toEqual('ToDoAppAngularUI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, todoapp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ToDoAppAngularUI');
   });
 });
