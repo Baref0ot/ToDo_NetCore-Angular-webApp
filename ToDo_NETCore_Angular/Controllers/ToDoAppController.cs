1d3d7750af957a5b1d8732fccde91b5b64b33243
baref0ot <Mattwright700@yahoo.com>
2023-12-13 20:01:45 -0500

    Added a web API Controller.

---
+++ ToDo_NETCore_Angular/Controllers/ToDoAppController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo_NETCore_Angular.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoAppController : ControllerBase {
+    }
+}
