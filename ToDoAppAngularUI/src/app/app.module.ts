e7fddb55205cf178895acf52b5f135e0d1fcd11f
baref0ot <Mattwright700@yahoo.com>
2023-12-14 03:16:58 -0500

    Change "HttpClient" to "HttpClientModule"

--- ToDoAppAngularUI/src/app/app.module.ts
+++ ToDoAppAngularUI/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClient} from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { HttpClient} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClient
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
